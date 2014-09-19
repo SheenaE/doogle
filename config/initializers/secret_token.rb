4bbc9517a43ddeec2510a18943660b096141772d
Sheena Ellenburg <sheena.or@gmail.com>
2014-09-18 23:21:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doogle::Application.config.secret_key_base = '75bd47063b9272a2c06ad586adc23d4c000fd9627da27aaa430925d39b996c4fd1baa18f7037c5215d81e154825876c03d3314f2263b29d1aebf3accbc276b95'
