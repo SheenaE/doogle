e24c7be407884ddb47f2c1a65cee444359f85f28
Sheena Ellenburg <sheena.or@gmail.com>
2014-09-22 03:30:59 -0700

    added landing page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Doogle::Application.routes.draw do
+  get "landing_page/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
