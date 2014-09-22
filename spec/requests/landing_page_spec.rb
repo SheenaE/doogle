e11ad26ba3d8a7ac44b6ff4e6c62b0b7842b22f3
Sheena Ellenburg <sheena.or@gmail.com>
2014-09-22 04:52:50 -0700

    basic landing page spec and components

--- spec/requests/landing_page_spec.rb
+++ spec/requests/landing_page_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 describe "Landing_pages" do
-  it "should have the content 'Doogle'" do
+  before do
     visit root_path
+  end
+
+  it "should have the heading 'Doogle'" do
     expect(page).to have_content('Doogle')
+  end
+
+  it "should have input text field" do
+    expect(page).to have_field('word', :type => 'text')
+  end
+
+  it "should have a submit button" do
+    expect(page).to have_button('Submit') 
   end
 end
